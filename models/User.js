8f7e8cfb2cdb7a0177432ffd5093ca92d979e9cf
Ayan <ayan@gmail.com>
2025-05-01 08:55:13 +0530

    first

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/login")
+mongoose.connect("mongodb://localhost:27017/login") 
 
 
 const userSchema = new mongoose.Schema({
